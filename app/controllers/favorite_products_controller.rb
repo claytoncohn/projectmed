628a336d6043010db1b01fc1f99ff46af12607de
claytoncohn <clayton@claytoncohn.com>
2016-06-01 15:21:14 -0500

    minor tweaks to favorite products

--- app/controllers/favorite_products_controller.rb
+++ app/controllers/favorite_products_controller.rb
@@ -3,15 +3,15 @@ class FavoriteProductsController < ApplicationController
   
   def create
     if Favorite.create(favorited: @product, user: current_user)
-      redirect_to @product, notice: 'product has been favorited'
+      redirect_to @product, notice: 'Product has been favorited.'
     else
-      redirect_to @product, alert: 'Something went wrong'
+      redirect_to @product, alert: 'Unable to favorite.'
     end
   end
   
   def destroy
     Favorite.where(favorited_id: @product.id, user_id: current_user.id).first.destroy
-    redirect_to @product, notice: 'product is no longer in favorites'
+    redirect_to @product, notice: 'Product has been removed from favorites.'
   end
   
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :watches
   has_many :watchers, :through => :watches
-  has_attached_file :image, styles: {main: "800x300>", thumb: "320x150>"}
+
+has_attached_file :image, styles: {carousel:"800x300#", main: "800x300^", min: "320x150#", thumb: "200x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   ratyrate_rateable 'product_rating'
   def self.search(search)
